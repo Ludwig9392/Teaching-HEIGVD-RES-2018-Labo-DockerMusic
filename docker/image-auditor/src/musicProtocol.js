79e61dbc67118c5f2700e70654df2363724b3ded
Ludwig9392 <loic.frueh@gmail.com>
2018-05-18 01:30:12 +0200

    Begin of the project - added some line for the auditor and complete the packages JSON files

---
+++ docker/image-auditor/src/musicProtocol.js
@@ -0,0 +1,7 @@
+/*
+    Constants, like adresses and ports that are useful for the music application protocole.
+ */
+
+exports.MULTICAST_ADRESS = "299.255.22.5";
+exports.UDP_PORT = 2206;
+exports.TCP_PORT = 2205;
