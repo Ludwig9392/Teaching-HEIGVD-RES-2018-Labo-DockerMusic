4ba2f5be0f0e6d9261b7f0c37fe1347a30a0950d
Ludwig9392 <loic.frueh@gmail.com>
2018-05-18 01:33:21 +0200

    Begin of the project - added some line for the musician and complete the packages JSON files

---
+++ docker/image-musician/src/musicProtocol.js
@@ -0,0 +1,6 @@
+/*
+    Constants, like adresses and ports that are useful for the music application protocole.
+ */
+
+exports.MULTICAST_ADRESS = "299.255.22.5";
+exports.UDP_PORT = 2206;
---
+++ docker/image-musician/src/musician.js
@@ -0,0 +1,12 @@
+// Protocol recup.
+var protocol = require('musicProtocol');
+
+// Node.js module for UDP for UUID
+var uuid = require('uuid');
+
+// Node.js module for UDP
+var datagram = require('dgram');
+
+var sender = datagram.createSocket('udp4');
+
+
