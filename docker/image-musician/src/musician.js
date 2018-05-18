eac559d4ae32db50c71f6484cc92c9fe77535f8b
Ludwig9392 <loic.frueh@gmail.com>
2018-05-18 02:29:20 +0200

    First implementation of musician.js

--- docker/image-musician/src/musician.js
+++ docker/image-musician/src/musician.js
@@ -9,4 +9,39 @@ var datagram = require('dgram');
 
 var sender = datagram.createSocket('udp4');
 
+// Initialisation of the instruments table (like in the SPEC)
+var instruments = new Map();
+instruments.set("piano", "ti-ta-ti");
+instruments.set("trumpet", "pouet");
+instruments.set("flute", "trulu");
+instruments.set("violin", "gzi-gzi");
+instruments.set("drum", "boum-boum");
 
+// user Argument -> instrument
+var instrument = process.argv[2];
+
+//if the instrument doesn't exist, we quit the process
+if(instruments.get(instrument) == undefined){
+    process.on('exit', function(){
+        console.log("Invalid instrument");
+        process.exit(1);
+    });
+}
+
+var musician = {
+    "uuid" : uuid(),
+    "instrument" : instrument,
+    "activeSince" : new Date()
+};
+
+var payload = JSON.stringify(musician);
+
+// Let's send this payload to all the subscriber of the multicast adresse
+message = new Buffer(payload);
+var update = function() {
+    sender.send(message, 0, message.length, protocol.UDP_PORT, protocol.MULTICAST_ADRESS, function () {
+        console.log("Sending payload: " + payload + " via port " + sender.address().port);
+    });
+}
+// And let's do this every second
+setInterval(update, 1000);
